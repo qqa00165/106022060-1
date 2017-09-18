5f8e560617bbc73c2c717adc84ee7e35e8c1008e
qqa00165 <qqa00165@gmail.com>
2017-09-18 15:16:39 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("學號:106022060");
+        System.out.println("系級：行動商務與多媒體應用學系一年級");
+        System.out.println("姓名：何薇儀");
+        System.out.println("時間：2016/09/18");
+        System.out.print("地點：\"I319\"");
+	// write your code here
+    }
+}
